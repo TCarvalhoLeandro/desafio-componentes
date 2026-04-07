e6128fd948c83906a153be4ed118a83e60e0af35
Leandro <trairicarvalho@gmail.com>
2026-04-06 23:26:25 -0300

    Implement: implementa interface CommandRunLine

--- src/main/java/com/leandro/desafio_componentes/DesafioComponentesApplication.java
+++ src/main/java/com/leandro/desafio_componentes/DesafioComponentesApplication.java
@@ -1,13 +1,20 @@
 package com.leandro.desafio_componentes;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DesafioComponentesApplication {
+public class DesafioComponentesApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(DesafioComponentesApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Hello World!!");
+		
 	}
 
 }
