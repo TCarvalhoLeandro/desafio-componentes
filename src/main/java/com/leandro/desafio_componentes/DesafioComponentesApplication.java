7b9d4309d83c550896794508fee4d80edd92c81c
Leandro <trairicarvalho@gmail.com>
2026-04-06 23:18:38 -0300

    first: cria projeto spring

---
+++ src/main/java/com/leandro/desafio_componentes/DesafioComponentesApplication.java
@@ -0,0 +1,13 @@
+package com.leandro.desafio_componentes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioComponentesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioComponentesApplication.class, args);
+	}
+
+}
